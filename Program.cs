0905bd15505f78f23c12bab81bf7354572a90b47
Frank Diaz <diazfrankp@gmail.com>
2018-12-17 18:29:41 -0500

    added db context

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntroToOrmsPartTwo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
+//dotnet ef dbcontext scaffold "server=localhost;database=VacationSafari" Npgsql.EntityFrameworkCore.PostgreSQL -c VacationSafariContext
---
+++ VacationSafariContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IntroToOrmsPartTwo
+{
+  public partial class VacationSafariContext : DbContext
+  {
+    public VacationSafariContext()
+    {
+    }
+
+    public VacationSafariContext(DbContextOptions<VacationSafariContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseNpgsql("server=localhost;database=VacationSafari");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+    }
+  }
+}
