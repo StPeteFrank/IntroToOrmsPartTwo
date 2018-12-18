8ba62de45a10a3a2e2acddc020b584d40247f5b2
Frank Diaz <diazfrankp@gmail.com>
2018-12-17 19:37:14 -0500

    Created data through hardcode and user input.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,52 @@ namespace IntroToOrmsPartTwo
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      //Create data
+      var db = new VacationSafariContext();
+
+      //   db.SeenAnimals.Add(new SeenAnimals
+      //   {
+      //     Species = "Lions",
+      //     CountOfTimesSeen = 10,
+      //     LocationOfLastSeen = "Desert"
+      //   });
+
+      //   db.SaveChanges();
+      //   db.SeenAnimals.Add(new SeenAnimals
+      //   {
+      //     Species = "Tigers",
+      //     CountOfTimesSeen = 20,
+      //     LocationOfLastSeen = "Jungle"
+      //   });
+      //   db.SeenAnimals.Add(new SeenAnimals
+      //   {
+      //     Species = "Bears",
+      //     CountOfTimesSeen = 30,
+      //     LocationOfLastSeen = "Jungle"
+      //   });
+
+      //   db.SaveChanges();
+
+      //   db.SaveChanges();
+
+      System.Console.WriteLine("Enter the SeenAnimals details");
+      var Species = Console.ReadLine();
+      var CountOfTimesSeen = int.Parse(Console.ReadLine());
+      var LocationOfLastSeen = Console.ReadLine();
+
+      db.SeenAnimals.Add(new SeenAnimals
+      {
+        Species = Species,
+        CountOfTimesSeen = CountOfTimesSeen,
+        LocationOfLastSeen = LocationOfLastSeen
+      });
+
+      db.SaveChanges();
+
+
+      //READ data
+      //UPDATE data
+      //DELETE data
     }
   }
 }
